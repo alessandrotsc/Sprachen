9d95fc5d2f5795960b6d675e7a8859eb21a43fee
alessandrotsc <marcel@tasca-group.com>
2026-07-11 19:47:44 +0200

    Sprachen-Lern-App (PWA): Italienisch Basis 1/2/Urlaub

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='lernen-76be8652';
+const ASSETS=['./','index.html','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
